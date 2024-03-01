43a35214b10826f8dbb2d85c414fdfd811c6e7ac
Davi Garms <davi.garms@spektrix.com>
2024-03-01 16:29:36 +0000

    Add Playlist Type

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
 using Odyssey.MusicMatcher;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddGraphQLServer()
-    .AddQueryType<Query>();
+builder.Services
+    .AddCors(options =>
+    {
+        options.AddDefaultPolicy(builder =>
+        {
+            builder
+                .WithOrigins("https://studio.apollographql.com")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+    })
+    .AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddType<Playlist>();
 
 var app = builder.Build();
 
 //http://localhost:5059/GraphQL/
+app.UseCors();
 app.MapGraphQL();
-
 app.Run();
 
