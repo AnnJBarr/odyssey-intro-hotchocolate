bd3c24a95ff8abab96fedde442136e0685bac8fe
Ed Reeve <49102237+edwardreeve@users.noreply.github.com>
2024-03-06 10:32:50 +0000

    add primary constructor to Playlist class; replace hello world query with featured playlists

--- Types/Playlist.cs
+++ Types/Playlist.cs
@@ -1,15 +1,15 @@
 namespace Odyssey.MusicMatcher;
 
 [GraphQLDescription("A curated collection of tracks designed for a specific activity or mood.")]
-public class Playlist
+public class Playlist(string id, string name, string description)
 {
   [GraphQLDescription("The ID for the playlist.")]
   [ID]
-  public string Id { get; }
+  public string Id { get; } = id;
 
   [GraphQLDescription("The name of the playlist.")]
-  public string Name { get; set; }
+  public string Name { get; set; } = name;
 
   [GraphQLDescription("Describes the playlist, what to expect and entices the user to listen.")]
-  public string? Description { get; set; }
+  public string? Description { get; set; } = description;
 }
--- Types/Query.cs
+++ Types/Query.cs
@@ -2,9 +2,14 @@ namespace Odyssey.MusicMatcher;
 
 public class Query
 {
-    // where Query resolver functions will go
-    public string Hello()
+    [GraphQLDescription("Returns some AMAZING playlists, not awful ones - all awesome")]
+    public List<Playlist> FeaturedPlaylists()
     {
-        return "Hello world";
-    }    
+        return
+        [
+            new Playlist("1", "Suggestive Reading", "for those quiet moments"),
+            new Playlist("2", "No Swears", "suitable for car trips with young relatives"),
+            new Playlist("3", "Hip Hop, Grime", "does what it says on the tin")
+        ];
+    }
 }
